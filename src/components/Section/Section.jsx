c21f85e5f30abeebea24d317d8f498c4a2aa3435
NickNedilko <overlord311089@gmail.com>
2023-03-04 07:51:23 +0200

    changes
    asdsfg

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Section = title => {
+  console.log(title);
+  return (
+    <section>
+      <h1>{title}</h1>
+    </section>
+  );
+};
+
+export default Section;
